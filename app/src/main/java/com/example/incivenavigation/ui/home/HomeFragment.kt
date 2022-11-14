c3666eba11493abcdc038e5b4171936cddb66950
pabarqlop <pabloarques98@gmail.com>
2022-11-14 09:57:35 +0100

    permission

--- app/src/main/java/com/example/incivenavigation/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/incivenavigation/ui/home/HomeFragment.kt
@@ -1,10 +1,15 @@
 package com.example.incivenavigation.ui.home
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.incivenavigation.databinding.FragmentHomeBinding
@@ -16,7 +21,7 @@ private var _binding: FragmentHomeBinding? = null
   // onDestroyView.
   private val binding get() = _binding!!
 
-  override fun onCreateView(
+    override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
     savedInstanceState: Bundle?
@@ -27,9 +32,27 @@ private var _binding: FragmentHomeBinding? = null
     _binding = FragmentHomeBinding.inflate(inflater, container, false)
     val root: View = binding.root
 
+      binding.buttonlocation.setOnClickListener(){
+
+      }
+
 
     return root
   }
+
+    private fun getLocation() {
+        if(ContextCompat.checkSelfPermission(requireContext(),Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED){
+           Toast.makeText(requireContext(), "Request permissions", Toast.LENGTH_SHORT).show()
+
+            locationPermissionRequest.launch(new String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            });
+        } else {
+            Toast.makeText(requireContext(), "getLocation: permissions granted", Toast.LENGTH_SHORT).show();
+        }
+    }
 
 override fun onDestroyView() {
         super.onDestroyView()
